cfc3fb18bb610b879c5fdd8c0e7e4890285eb690
Andriy Bun <andr.bun@gmail.com>
2013-12-10 15:09:55 +0100

    Bioreactor model changed

--- Integrated Model/Travel Times Concept Integrated/MSWS1/inhibition.m
+++ Integrated Model/Travel Times Concept Integrated/MSWS1/inhibition.m
@@ -26,13 +26,13 @@ f3(Rp.inhib(k1,2)) = Ki.^2./(Ki.^2 + Ci.^2);    % Siegrist et al 2002
 k1 = find(Rp.inhib(:,1) == 4);     
 Ki = Rp.inhib(k1,4); 
 Ci = CT(Rp.inhib(k1,3));
-f4(Rp.inhib(k1,2)) = (1-Ci./Ki).^0.401;  
+% f4(Rp.inhib(k1,2)) = (1-Ci./Ki).^0.401;  
 
 % f5:
 k1 = find(Rp.inhib(:,1) == 5);     
 Ki = Rp.inhib(k1,4); 
 Ci = CT(Rp.inhib(k1,3));
-f5(Rp.inhib(k1,2)) = (1+(Ci./Ki).^1.08).^-1;  
+% f5(Rp.inhib(k1,2)) = (1+(Ci./Ki).^1.08).^-1;  
 
 % I
 I = f1.*f2.*f3.*f4.*f5;
