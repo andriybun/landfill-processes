63405b28025a1a95dc34c2cb509ef29be0c0f364
Andriy Bun <andr.bun@gmail.com>
2013-12-12 16:30:44 +0100

    Updated GenerateCharacteristicSuffix.m: removed mention of kExchPart

--- Integrated Model/Travel Times Concept Integrated/Transport/GenerateCharacteristicSuffix.m
+++ Integrated Model/Travel Times Concept Integrated/Transport/GenerateCharacteristicSuffix.m
@@ -1,4 +1,4 @@
 function fileNameSuffix = GenerateCharacteristicSuffix(ModelParams, ParameterOfInterest)
-    fileNameSuffix = sprintf('beta_%3.2f_kEx_%4.3f_kExPart_%4.3f_(%s)', ...
-        ModelParams.beta, ModelParams.kExch, ModelParams.kExchPart, ParameterOfInterest.name);
+    fileNameSuffix = sprintf('beta_%3.2f_kEx_%4.3f_(%s)', ...
+        ModelParams.beta, ModelParams.kExch, ParameterOfInterest.name);
 end
