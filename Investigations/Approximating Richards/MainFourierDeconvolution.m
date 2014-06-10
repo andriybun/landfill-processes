12dae4aee4b60da9b428a35d623a4ca7e5f8b44e
Andriy Bun <andr.bun@gmail.com>
2014-06-10 15:26:37 +0200

    Improving deconvolution scripts

--- Investigations/Approximating Richards/MainFourierDeconvolution.m
+++ Investigations/Approximating Richards/MainFourierDeconvolution.m
@@ -6,7 +6,10 @@ function MainFourierDeconvolution
     
     MAT_FILE_DIR = '../Transforms/mat/';
 %     CASE_NAME = 'Richards_Output_Pulse_Very_Slow_Response_zref_-1.0.mat';
-    CASE_NAME = 'Richards_Output_Very_Slow_Response.mat'; % Sin, Square_Wave
+%     CASE_NAME = 'Richards_Output_Very_Slow_Response.mat'; % Sin, Square_Wave
+    caseNameVector = {'Extreme', 'Middle_Extreme', 'Netherlands', 'Uniform'};
+    caseIdx = 3;
+    CASE_NAME = sprintf('Richards_Output_rainfall_%s.mat', caseNameVector{caseIdx});
     
     RawData = load([MAT_FILE_DIR CASE_NAME]);
     
@@ -38,25 +41,26 @@ function MainFourierDeconvolution
     % Inverse Fourier transform to get approximation of probability density function
     pdfEst = smooth(ifft(pdfF), 1);
 
-%     muEst = 9.6543;
-%     sigmaEst = 0.6;
+
 %     pdfEst = lognpdfX(t, muEst, sigmaEst, dt);
     
     resSel = 1:40; % ':';
 %     plot(pdfEst(resSel));
     pdfEstAdj = pdfEst / sum(pdfEst(resSel));
     [muEst, sigmaEst] = CalcLognormMuSigma(t(resSel), pdfEstAdj(resSel));
-    muEst = muEst - 0.;
-    sigmaEst = sigmaEst + 0.;
+%     muEst = 9.4;
+%     sigmaEst = 0.55;
     fprintf('%f\t%f\n', muEst, sigmaEst);
     
     % Calculate numerical convolutions
+    qOutConvLognVar = NumericalConvolution(t, qIn, pdfEstAdj);
+    
     qOutConvLogn = NumericalConvolution(t, qIn, ...
         @(t, mu, sigma) lognpdfX(t, mu, sigma, dt), muEst, sigmaEst);
     thetaIni = 0.157630;
     vPore = 0.45 / 3;
-    [qOutConvLognVar, dTheta, dMu, dSigma] = NumericalConvolution(t, qIn, ...
-        @(t, mu, sigma) lognpdfX(t, mu, sigma, dt), @LogNormalParams, thetaIni, vPore);
+%     [qOutConvLognVar, dTheta, dMu, dSigma] = NumericalConvolution(t, qIn, ...
+%         @(t, mu, sigma) lognpdfX(t, mu, sigma, dt), @LogNormalParams, thetaIni, vPore);
 %     plot(t, dTheta);
 
 %%
@@ -81,9 +85,9 @@ function MainFourierDeconvolution
     % Plot
     fH = figure(2);
     set(fH, 'Position', [500, 400, 700, 350]);
-    plotyy(t, qIn, t, cat(2, qOut, qOutConvLogn, qOutConvLognVar));
-    legend('Influx', 'Real data', 'Convolution (lognormal)', ...
-        'Convolution (lognormal, variable)', 'Location', 'Northwest');
+    plotyy(t, qIn, t, cat(2, qOutConvLogn, qOutConvLognVar, qOut));
+    legend('Influx', 'Convolution (lognormal)', 'Convolution (lognormal, variable)', ...
+        'Real data', 'Location', 'Northwest');
     xlabel('Time, minutes');
     ylabel('In flux');
     % Get handles of lines and change their styles
@@ -94,11 +98,11 @@ function MainFourierDeconvolution
     set(axH(3), {'ycolor'}, {qInColor / 2});
     set(get(axH(2), 'ylabel'), 'string', 'Out flux');
     set(axH(2), 'ylim', [0, max(cat(1, qOut, qOutConvLogn, qOutConvLognVar)) * 1.05]);
-    set(axH(2), 'xlim', [t(1), t(end)]);
+    set(axH(2), 'xlim', [t(1), t(end)]); % [3, 5.24] * 1e5);
     set(axH(3), 'xlim', [t(1), t(end)]);
-    set(lH{2}(1), 'Color', [0.3, 0.3, 0.3], 'LineStyle', '--');
-    set(lH{2}(2), 'Color', [0.6, 0.6, 0.6]);
-    set(lH{2}(3), 'Color', [0., 0., 0.], 'LineWidth', 2);
+    set(lH{2}(1), 'Color', [0., 0., 0.], 'LineWidth', 1);
+    set(lH{2}(2), 'Color', [0.5, 0.5, 0.5], 'LineStyle', '--', 'LineWidth', 2);
+    set(lH{2}(3), 'Color', [0.7, 0.7, 0.7], 'LineWidth', 2);
     hgsave(fH, sprintf('./fig/%s_(de)convolution.fig', CASE_NAME));
     
     return
@@ -107,16 +111,53 @@ function MainFourierDeconvolution
         pdf = logncdf([t; t(end)+dt], mu, sigma);
         pdf = diff(pdf);
     end
-    
-    function [mux, sigmax] = CalcLognormMuSigma(x, pdf)
-            meanEst = sum(pdf .* x);
-            varEst = sum(pdf .* x.^2) - meanEst ^ 2;
-            [mux, sigmax] = CalcLognormMuSigmaFromMoments(meanEst, varEst);
+
+    function [optMu, optSigma, optErr] = CalcLognormMuSigma(x, pdf)
+        DELTA_MU = 2e-2;
+        DELTA_SIGMA = 5e-3;
+        RANGE_MU = [2, 5];
+        RANGE_SIGMA = [0.8, 1.2];
+        NUM_SIGMAS_CONFIDENCE = 4;
+        ERR_EPSILON = 3e-4;
+        f = @(x) 1; % (1 - erf((x - 4.e+4) / 1.e+4)) / 2;
+        pdf = pdf .* f(x);
+        pdf = max(0, pdf); %  / sum(pdf);
+        optErr = Inf;
+        optMu = nan;
+        optSigma = nan;
+        for mu_ = RANGE_MU(1):DELTA_MU:RANGE_MU(2);
+            for sigma_ = RANGE_SIGMA(1):DELTA_SIGMA:RANGE_SIGMA(2)
+                err = SumSquares(pdf, lognpdfX(x, mu_, sigma_, dt));
+                if (err < optErr)
+                    optErr = err;
+                    optMu = mu_;
+                    optSigma = sigma_;
+                end
+            end
+        end
+%         close all;
+%         plotyy(x, pdf, x, f(x));
+%         hold on
+%         plot(x, lognpdfX(x, optMu, optSigma, dt), 'r');
+%         hold off
     end
-    
-    function [mux, sigmax] = CalcLognormMuSigmaFromMoments(evn, varn)
-        sigmax = sqrt(log(varn ./ (evn .* evn) + 1));
-        mux = log(evn) - sigmax .* sigmax ./ 2;
+
+    function err_ = SumSquares(v1, v2, k)
+        if nargin < 3
+            k = ones(size(v1));
+        end
+        err_ = sum((k .* (v1 - v2)) .^ 2);
     end
+
+%     function [mux, sigmax] = CalcLognormMuSigma(x, pdf)
+%             meanEst = sum(pdf .* x);
+%             varEst = sum(pdf .* x.^2) - meanEst ^ 2;
+%             [mux, sigmax] = CalcLognormMuSigmaFromMoments(meanEst, varEst);
+%     end
+%     
+%     function [mux, sigmax] = CalcLognormMuSigmaFromMoments(evn, varn)
+%         sigmax = sqrt(log(varn ./ (evn .* evn) + 1));
+%         mux = log(evn) - sigmax .* sigmax ./ 2;
+%     end
 
 end
