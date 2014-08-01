18559cf3848ae3cb9beddee20c0c29cfacad3939
Andriy Bun <andr.bun@gmail.com>
2014-08-01 15:35:12 +0200

    Workaround to fix issue with Matlab clearing some variables when initializing Orchestra

--- Integrated Model/Travel Times Concept Integrated/MSWS1/initialize_ORI.m
+++ Integrated Model/Travel Times Concept Integrated/MSWS1/initialize_ORI.m
@@ -1,12 +1,25 @@
 function ORI = initialize_ORI(Comp,x)
-% Specify the path to a compiled JAR file with the ORCHESTRA interface.
-% And import corresponding libraries
-javaclasspath([cd '/../Orchestra/Interface/OrchestraInterface.jar']);
-import OrchestraInterface.*;
+    % Specify the path to a compiled JAR file with the ORCHESTRA interface.
+    % And import corresponding libraries
+    ORCHESTRA_FOLDER = [cd '/../Orchestra/Interface/OrchestraInterface.jar'];
+    javaclasspath('-v1');
+    jp = javaclasspath;
+    jpFull = cell(size(jp));
+    for i = 1:numel(jp)
+        jpFull{i} = which(jp{i});
+    end
+    if ~ismember(which(ORCHESTRA_FOLDER), jpFull)
+        warning(['Adding Orchestra to Matlab path. This may cause your program to misbehave. ' ...
+            'In such case stry restarting your program without rebooting Matlab.']);
+        javaaddpath(ORCHESTRA_FOLDER);
+    end
+    
+    % javaaddpath(ORCHESTRA_FOLDER);
+    import OrchestraInterface.*;
 
-% Initialize a list of Master species and I/O variables
-% Give each variable a name, default value, indicate if it is a static variable
-% and indicate where this variable was defined.
+    % Initialize a list of Master species and I/O variables
+    % Give each variable a name, default value, indicate if it is a static variable
+    % and indicate where this variable was defined.
     variableList = NodeVariableInfoList(length(Comp.all));
     for i = 1:length(Comp.all)
         variableList.SetVariable(i, Comp.all(i), Comp.alli(i), false, 'defined by example');
@@ -18,15 +31,15 @@ import OrchestraInterface.*;
         activate_activity = ORI.Calculate(1:length(Comp.masteri), [Comp.masteri]);
     end
     
-% Initialize ORCHESTRA object
-% Initialize H2CO3.tot and H+.tot with initial pH2CO3 and pH
+    % Initialize ORCHESTRA object
+    % Initialize H2CO3.tot and H+.tot with initial pH2CO3 and pH
     if x == 1
         
         ORI = OrchestraModule([cd '/../Orchestra/Initialize/chemistry.inp'], variableList, ioVariableList);
         ORI = ORI.Calculate(1:length(Comp.masteri), [Comp.masteri]);
     end
-
-% Initialize ORCHESTRA with total amounts of all master species
+    
+    % Initialize ORCHESTRA with total amounts of all master species
     if x == 2
         ORI = OrchestraModule([cd '/../Orchestra/Bioreactor/chemistry.inp'], variableList, ioVariableList);
         ORI = ORI.Calculate(1:length(Comp.masteri), [Comp.masteri]);
